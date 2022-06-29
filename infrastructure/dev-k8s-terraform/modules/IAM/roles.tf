4b4578456bda8c314a748d5c877bc7dba0daccf2
aakcay061954 <abdullahakcay84@gmail.com>
2022-06-29 18:24:55 +0000

    added ansible dynamic inventory files for dev environment

--- infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
@@ -68,12 +68,12 @@ resource "aws_iam_policy_attachment" "attach_for_worker" {
   policy_arn = aws_iam_policy.policy_for_worker_role.arn
 }
 
-resource "aws_iam_instance_profile" "profile_for_master" {
+resource "aws_iam_instance_profile" "profile_for_master-aa" {
   name  = "profile_for_master"
   role = aws_iam_role.role_for_master.name
 }
 
-resource "aws_iam_instance_profile" "profile_for_worker" {
+resource "aws_iam_instance_profile" "profile_for_worker-aa" {
   name  = "profile_for_worker"
   role = aws_iam_role.role_for_worker.name
 }
